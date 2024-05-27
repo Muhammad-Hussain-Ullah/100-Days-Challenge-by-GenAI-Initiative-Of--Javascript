31d9198c2f8c8dde999dcda4a2b5bdaeed7d05a3
Muhammad Hussain Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-05-27 09:08:42 +0500

    Add files via upload

---
+++ Day1Question3.js
@@ -0,0 +1,7 @@
+"use strict";
+let personname = "husSain";
+console.log(personname.toLowerCase());
+console.log(personname.toUpperCase());
+console.log(personname.slice(1).toLowerCase());
+//in line 4 personname.charAt(0) it will take control of the first letter .toUpperCase will make the first letter
+//capital here + is concatinating  
